9914ff51e52a50ef122d23b3a775b32d5fe63c9e
turtle-engineer123 <maximilianschonl793@gmail.com>
2026-06-28 16:13:24 +0200

    we invaded france again through belgium

--- game.py
+++ game.py
@@ -1,40 +1,77 @@
 import arcade
+import math
 
-# A tower is a thing we put on the screen to help defend the path.
 class Tower(arcade.Sprite):
     def __init__(self, x, y):
-        # Use the picture place.png for the tower.
         super().__init__("place.png")
-        # Put the tower where the mouse clicked.
-        self.position = (x, y)
-        # Keep the tower at normal size.
+        self.center_x = x
+        self.center_y = y
         self.scale = 1
+        self.time_since_last_shot = 0.0
 
+class Enemy(arcade.Sprite):
+    def __init__(self, path_points):
+        super().__init__("enemy.png")
+        self.path_points = path_points
+        self.current_target = 1
+        self.health = 5
+        self.speed = 120
+        self.center_x, self.center_y = path_points[0]
 
-# This is the game window where the path and towers are shown.
+    def update(self, delta_time):
+        if self.current_target >= len(self.path_points):
+            return
+        target_x, target_y = self.path_points[self.current_target]
+        dx = target_x - self.center_x
+        dy = target_y - self.center_y
+        distance = math.hypot(dx, dy)
+        if distance == 0:
+            self.current_target += 1
+            return
+        move_distance = self.speed * delta_time
+        if move_distance >= distance:
+            self.center_x = target_x
+            self.center_y = target_y
+            self.current_target += 1
+        else:
+            self.center_x += dx / distance * move_distance
+            self.center_y += dy / distance * move_distance
+
+class Bullet(arcade.Sprite):
+    def __init__(self, x, y, dx, dy):
+        super().__init__("bullet.png")
+        self.center_x = x
+        self.center_y = y
+        self.scale = 0.4
+        self.velocity_x = dx
+        self.velocity_y = dy
+
+    def update(self, delta_time):
+        self.center_x += self.velocity_x * delta_time
+        self.center_y += self.velocity_y * delta_time
+
 class TowerDefense(arcade.Window):
 
     def __init__(self):
-        # Make a window that is 1500 pixels wide and 800 pixels tall.
         super().__init__(1500,800,("accurate defense simulator"))
 
-        # Set the background to brown dirt color.
         arcade.set_background_color(arcade.color.DIRT)
 
-        # Lists to keep track of path pieces, towers, and UI buttons.
         self.paths = arcade.SpriteList()
         self.towers = arcade.SpriteList()
+        self.enemies = arcade.SpriteList()
+        self.bullets = arcade.SpriteList()
         self.buttons = arcade.SpriteList()
         self.placing_mode = False
+        self.path_positions = []
+        self.spawn_timer = 0.0
 
-        # Preview sprite while in placing mode.
         self.preview = arcade.Sprite("place.png")
         self.preview.scale = 1
         self.preview.alpha = 120
         self.preview.color = arcade.color.WHITE
         self.preview.position = (0, 0)
 
-        # Button to toggle placing mode.
         self.button_texture = arcade.load_texture("mutton.png")
         self.button_exit_texture = arcade.load_texture("exit.png")
         self.button = arcade.Sprite("mutton.png")
@@ -42,47 +79,44 @@ class TowerDefense(arcade.Window):
         self.button.position = (50, 50)
         self.buttons.append(self.button)
 
-        # Start the path at the top-left area.
         self.pathx = 50
         self.pathy = 650
         self.money = 500
 
-        # Add straight path pieces.
-        def Path(repeat, angle2set):
-            for i in range(repeat):
-                path = arcade.Sprite("gerade.png")
-                path.scale = 2
-                path.position = (self.pathx, self.pathy)
-                path.angle = angle2set
-                # Move the next path piece to the right place.
-                if angle2set == 0:
-                    self.pathx += 100
-                elif angle2set == 90:
-                    self.pathy += 100
-                elif angle2set == 180:
-                    self.pathx -= 100
-                elif angle2set == 270:
-                    self.pathy -= 100
-                self.paths.append(path)
-
-        # Add a curved path piece.
-        def Curve(angle, adjustposition):
-            path = arcade.Sprite("curve.png")
+        def add_path_tile(texture, angle, advance_x, advance_y):
+            path = arcade.Sprite(texture)
             path.scale = 2
             path.position = (self.pathx, self.pathy)
             path.angle = angle
-            # Move after placing the curve.
-            if adjustposition == 1:
-                self.pathx += 100
-            elif adjustposition == 2:
-                self.pathy += 100
-            elif adjustposition == 3:
-                self.pathx -= 100
-            elif adjustposition == 4:
-                self.pathy -= 100
             self.paths.append(path)
+            self.path_positions.append((self.pathx, self.pathy))
+            self.pathx += advance_x
+            self.pathy += advance_y
 
-        # Create the path shape using straight and curved pieces.
+        def Path(repeat, angle2set):
+            for _ in range(repeat):
+                if angle2set == 0:
+                    add_path_tile("gerade.png", 0, 100, 0)
+                elif angle2set == 90:
+                    add_path_tile("gerade.png", 90, 0, 100)
+                elif angle2set == 180:
+                    add_path_tile("gerade.png", 180, -100, 0)
+                elif angle2set == 270:
+                    add_path_tile("gerade.png", 270, 0, -100)
+
+        def Curve(angle, adjustposition):
+            advance_x = 0
+            advance_y = 0
+            if adjustposition == 1:
+                advance_x = 100
+            elif adjustposition == 2:
+                advance_y = 100
+            elif adjustposition == 3:
+                advance_x = -100
+            elif adjustposition == 4:
+                advance_y = -100
+            add_path_tile("curve.png", angle, advance_x, advance_y)
+
         Path(4,0)
         Curve(270,4)
         Path(2,270)
@@ -101,16 +135,11 @@ class TowerDefense(arcade.Window):
         Path(1,0)
 
 
-
-
-
-        
-        
-
-    # Draw the game screen each time it updates.
     def on_draw(self):
         self.clear()
         self.paths.draw()
+        self.enemies.draw()
+        self.bullets.draw()
         self.towers.draw()
         self.buttons.draw()
 
@@ -134,6 +163,7 @@ class TowerDefense(arcade.Window):
             self.preview.position = (x, y)
 
     def on_mouse_press(self, x, y, button, modifiers):
+        COIN_SOUND = arcade.load_sound("ksjsbwuil-cash-register-1-513922.mp3")
         if button == arcade.MOUSE_BUTTON_LEFT and self.button.collides_with_point((x, y)):
             self.placing_mode = not self.placing_mode
             if self.placing_mode:
@@ -151,10 +181,75 @@ class TowerDefense(arcade.Window):
                 else:
                     self.towers.append(tower)
                     self.money -= 250
+                    self.coin_playback = COIN_SOUND.play()
+                    self.placing_mode = not self.placing_mode
             else:
                 print("Not enough money to place a tower!")
 
-# Start the game.
+    def get_nearest_enemy(self, sprite):
+        if len(self.enemies) == 0:
+            return None
+        return min(self.enemies, key=lambda enemy: arcade.get_distance_between_sprites(sprite, enemy))
+
+    def spawn_enemy(self):
+        if not self.path_positions:
+            return
+        enemy = Enemy(self.path_positions)
+        self.enemies.append(enemy)
+
+    def on_update(self, delta_time):
+        self.spawn_timer += delta_time
+        if self.spawn_timer >= 10.0:
+            self.spawn_timer -= 10.0
+            self.spawn_enemy()
+
+        for enemy in list(self.enemies):
+            enemy.update(delta_time)
+            if enemy.current_target >= len(enemy.path_points):
+                self.enemies.remove(enemy)
+
+        for bullet in list(self.bullets):
+            bullet.update(delta_time)
+            if (
+                bullet.center_x < 0
+                or bullet.center_x > self.width
+                or bullet.center_y < 0
+                or bullet.center_y > self.height
+            ):
+                self.bullets.remove(bullet)
+
+        for tower in self.towers:
+            target = self.get_nearest_enemy(tower)
+            if target:
+                dx = target.center_x - tower.center_x
+                dy = target.center_y - tower.center_y
+                tower.angle = math.degrees(math.atan2(dy, dx))
+                tower.time_since_last_shot += delta_time
+                if tower.time_since_last_shot >= 0.5:
+                    tower.time_since_last_shot -= 0.5
+                    distance = math.hypot(dx, dy)
+                    if distance > 0:
+                        direction_x = dx / distance
+                        direction_y = dy / distance
+                        bullet_speed = 400
+                        bullet = Bullet(
+                            tower.center_x,
+                            tower.center_y,
+                            direction_x * bullet_speed,
+                            direction_y * bullet_speed,
+                        )
+                        self.bullets.append(bullet)
+
+        for bullet in list(self.bullets):
+            enemies_hit = arcade.check_for_collision_with_list(bullet, self.enemies)
+            for enemy in enemies_hit:
+                if bullet in self.bullets:
+                    self.bullets.remove(bullet)
+                enemy.health -= 1
+                if enemy.health <= 0 and enemy in self.enemies:
+                    self.enemies.remove(enemy)
+                break
+
 TowerDefense()
 
 arcade.run()
